a4e998b416a5b17fe7b93cd2439849424a44a622
kyledowi <147605822+dowits01@users.noreply.github.com>
2025-07-27 11:00:45 +0800

    problem number 122

---
+++ Problems/_122BuyAndSelllStock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class _122BuyAndSelllStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if(prices[i] > prices[i - 1 ])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+
+                }                
+            }
+            return maxProfit;
+        }
+    }
+}
