31cbcb15a4c37c7f39afaa3eb82c4688e2a80ff1
kyledowi <147605822+dowits01@users.noreply.github.com>
2025-07-28 23:07:35 +0800

    Leetcode 26 and 20

---
+++ Problems/RemoveDup26.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class RemoveDup26
+    {
+        public int RemoveDuplicates(int[] num)
+        {
+            if(num.Length == 0) return 0;
+
+            int uniqueIndex = 1;
+
+            for(int i = 1; i < num.Length; i++)
+            {
+                if (num[i] != num[i - 1])
+                {
+                    num[uniqueIndex] = num[i];
+                    uniqueIndex++;
+                }
+            }
+            return uniqueIndex;
+        }
+    }
+}
