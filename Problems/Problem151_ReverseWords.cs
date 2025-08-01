2b675809f48cfd93c14a82f3fec4fa1c1ffb0401
kyledowi <147605822+dowits01@users.noreply.github.com>
2025-08-01 16:35:34 +0800

    Rename 151

---
+++ Problems/Problem151_ReverseWords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class Problem151_ReveseWords
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            Array.Reverse(words);
+            
+            return string.Join(" ", words);
+        }
+    }
+}
