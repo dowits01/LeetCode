c982b4a6b32d50b65dcc19b612e7e2a49e85a0e9
kyledowi <147605822+dowits01@users.noreply.github.com>
2025-08-01 15:51:47 +0800

    387. First Unique Character in a String

---
+++ Problems/FirstUniqChar387.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class FirstUniqChar387
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach(char c in s)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] += 1; 
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (dict[s[i]] == 1)
+                    return i;
+            }
+
+            return -1;
+        }
+        
+    }
+}
