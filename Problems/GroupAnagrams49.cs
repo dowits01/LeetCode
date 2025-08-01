bf327eca81a1f847820612d5773b25c23b3a5890
kyledowi <147605822+dowits01@users.noreply.github.com>
2025-08-01 15:37:10 +0800

    49. Group Anagrams

---
+++ Problems/GroupAnagrams49.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class GroupAnagrams49
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            if(strs.Length == 0)
+            {
+                return new List<IList<string>>();
+            }
+
+            Dictionary<string, List<string>> anagrams = new();
+
+            foreach( string word in strs )
+            {
+                int[] count = new int[26];
+                foreach(char c in word )
+                {
+                    count[c - 'a']++; 
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i <26; i++)
+                {
+                    sb.Append("#");
+                    sb.Append(count[i]);
+                }
+
+                string key = sb.ToString();
+
+                if(!anagrams.ContainsKey(key))
+                {
+                    anagrams[key] = new List<string>();
+
+                }
+
+                anagrams[key].Add(word);
+            }
+
+            return new List<IList<string>>(anagrams.Values);
+        }
+    }
+}
